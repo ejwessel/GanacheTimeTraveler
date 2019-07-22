77e4f12a31d8d78a81f6234dfcb25fb95ead38de
Ethan Wessel <ejwessel@gmail.com>
2019-07-22 19:46:42 -0400

    added base test methods

---
+++ tests/utils.test.js
@@ -0,0 +1,43 @@
+const TimeContract = artifacts.require('./TimeContract');
+const helper = require('utils.js');
+
+const SECONDS_IN_DAY = 86400;
+
+contract('TimeContract', async (accounts) =>  {
+
+    it("Test advanceTime", async() => {
+        //capture before time
+        await helper.advanceTime(SECONDS_IN_DAY)
+        //capture after time
+        //assert after time is later than before time by number of seconds
+    })
+
+    it("Test advanceBlock", async() => {
+        //capture block id before
+        await helper.advanceBlock()
+        //capture block id after
+        //assert that block is later than before block
+    })
+
+    it("Test advanceTimeAndBlock", async() => {
+        //capture before time
+        await helper.advanceTimeAndBlock(SECONDS_IN_DAY)
+        //capture after time
+        //assert after time is later than before time by number of seconds
+    })
+
+    it("Test takeSnapshot", async() => {
+        //capture id
+        snapShot = await helper.takeSnapshot();
+        snapshotId = snapShot['result'];
+        //assert there is an id
+    })
+
+    it("Test revertToSnapShot", async() => {
+        id = await helper.takeSnapshot()
+        //move time forward
+        //assert time moved
+        await helper.revertToSnapshot(id)
+        //assert time reverted
+    })
+});
