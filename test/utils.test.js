58cbc0a6b151448650627ddc5a90737138019194
brian <bchou515@gmail.com>
2019-07-24 17:55:31 -0400

    advanceBlock

--- test/utils.test.js
+++ test/utils.test.js
@@ -6,16 +6,36 @@ contract('Test Utils', async () =>  {
 
     it("Test advanceTime", async() => {
         //capture before time
+       
+        /*
+        const currentBlock = await web3.eth.getBlock()
+        console.log(currentBlock)
+
+        const output = await helper.advanceTime(SECONDS_IN_DAY);
+        console.log(output)
+        await web3.currentProvider.send({jsonrpc: "2.0", method: "evm_mine", params: [], id: 0})
+        
+        const advancedBlock = await web3.eth.getBlock(currentBlock.number)
+        console.log(advancedBlock)
+        assert.isBelow(currentBlock.timestamp, advancedBlock.timestamp, "Time was not advanced")
+        */
+
         //await helper.advanceTime(SECONDS_IN_DAY)
         //capture after time
         //assert after time is later than before time by number of seconds
+        // await helper.advanceTime(SECONDS_IN_DAY)
     })
 
     it("Test advanceBlock", async() => {
-        //capture block id before
-        //await helper.advanceBlock()
-        //capture block id after
-        //assert that block is later than before block
+        const currentBlock = await web3.eth.getBlock('latest')
+        const currentBlockNum = currentBlock.number
+
+        await helper.advanceBlock()
+        
+        const advancedBlock = await web3.eth.getBlock('latest')
+        const advanceBlockNum = advancedBlock.number
+        
+        assert.equal(currentBlockNum + 1, advanceBlockNum, "New block was not mined")
     })
 
     it("Test advanceTimeAndBlock", async() => {
