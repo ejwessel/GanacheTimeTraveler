c0e194bf107cf1272f0f35fec86465f56e4490e7
Ethan Wessel <ethan.wessel@fluidity.io>
2019-07-26 01:05:40 -0400

    groupings

--- test/utils.test.js
+++ test/utils.test.js
@@ -6,41 +6,43 @@ contract('Test Utils', async () =>  {
     it("Test advanceBlock", async() => {
         //grab block before advancing blocks 
         const blockBefore = await web3.eth.getBlock('latest')
-        const blockBeforeNum = blockBefore.number
+        const blockNumberBefore = blockBefore.number
 
         await helper.advanceBlock()
         
         //grab block after advancing blocks 
         const blockAfter = await web3.eth.getBlock('latest')
-        const advanceBlockNum = blockAfter.number
-        
-        assert.equal(blockBeforeNum + 1, advanceBlockNum, "New block was not mined")
+        const blockNumberAfter = blockAfter.number
+        assert.equal(blockNumberBefore + 1, blockNumberAfter, "New block was not mined")
     })
 
     it("Test advanceTime", async() => {
         const blockBefore = await web3.eth.getBlock('latest')
+        const timeBefore = blockBefore.timestamp
 
         await helper.advanceTime(SECONDS_IN_DAY);
         //time doesn't update unless block is mined
         await helper.advanceBlock()
         
         const blockAfter = await web3.eth.getBlock('latest')
-        assert.isBelow(blockBefore.timestamp, blockAfter.timestamp, "Time was not advanced")
+        const timeAfter = blockAfter.timestamp
+        assert.isBelow(timeBefore, timeAfter, "Time was not advanced")
     })
 
     it("Test advanceTimeAndBlock", async() => {
         const blockBefore = await web3.eth.getBlock('latest')
+        const timeBefore = blockBefore.timestamp
 
         await helper.advanceTimeAndBlock(SECONDS_IN_DAY)
 
         const blockAfter = await web3.eth.getBlock('latest')
-        assert.isBelow(blockBefore.timestamp, blockAfter.timestamp, "Time and Block was not advanced")
+        const timeAfter = blockAfter.timestamp
+        assert.isBelow(timeBefore, timeAfter, "Time and Block was not advanced")
     })
 
     it("Test takeSnapshot", async() => {
         const snapshot = await helper.takeSnapshot()
         const snapshotId = snapshot.result
-
         assert.exists(snapshotId, "Unable to produce snapshot")
     })
 
@@ -49,16 +51,19 @@ contract('Test Utils', async () =>  {
         const snapshot = await helper.takeSnapshot()
         const snapshotId = snapshot.result
         const blockBefore = await web3.eth.getBlock('latest')
+        const timeBefore = blockBefore.timestamp
 
         // advance time forward
         await helper.advanceTimeAndBlock(SECONDS_IN_DAY)
         const blockAfter = await web3.eth.getBlock('latest')
-        assert.isBelow(blockBefore.timestamp, blockAfter.timestamp, "Time did not advance")
+        const timeAfter = blockAfter.timestamp
+        assert.isBelow(timeBefore, timeAfter, "Time did not advance")
 
-        await helper.revertToSnapShot(snapshotId);
+        await helper.revertToSnapShot(snapshotId)
 
         //grab block after reverting time
-        const revertedBlock = await web3.eth.getBlock('latest')
-        assert.equal(blockBefore.timestamp, revertedBlock.timestamp, "Time and block have been reverted")
+        const blockReverted = await web3.eth.getBlock('latest')
+        const timeReverted = blockReverted.timestamp
+        assert.equal(timeBefore, timeReverted, "Time and block have been reverted")
     })
 });
