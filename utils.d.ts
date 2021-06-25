01a7b37352e734e9048e057ada24f16137d83201
Cody Nutt <cody.m.nutt@gmail.com>
2021-06-25 12:20:23 -0500

    jsdoc for typings

--- utils.d.ts
+++ utils.d.ts
@@ -7,11 +7,38 @@ declare module "ganache-time-traveler" {
         result?: any;
     }
 
+    /**
+     * Jump forward in time.
+     * @param time The amount of time to increase in seconds.
+     */    
     export function advanceTime(time: number): Promise<JsonRpcResponse>;
+
+    /**
+     * Force a block to be mined. Mines a block independent of whether or not mining is started or stopped.
+     */
     export function advanceBlock(): Promise<JsonRpcResponse>;
+
+    /**
+     * Force a block to be mined and set the time.
+     * @param time Set the clock to the specified time.
+     */
     export function advanceBlockAndSetTime(time: number): Promise<JsonRpcResponse>;
+
+    /**
+     * Force a block to be mined and increase the clock by the specified seconds.
+     * @param time The amount of time to increase in seconds.
+     */
     export function advanceTimeAndBlock(time: number): Promise<JsonRpcResponse>;
+
+    /**
+     * Snapshot the state of the blockchain at the current block.
+     */
     export function takeSnapshot(): Promise<JsonRpcResponse>;
+
+    /**
+     * Revert the state of the blockchain to a previous snapshot. 
+     * @param id Snapt id to revert to. This will be the 'result' property from takeSnapshot().
+     */
     export function revertToSnapshot(id: string): Promise<JsonRpcResponse>;
     
 }
